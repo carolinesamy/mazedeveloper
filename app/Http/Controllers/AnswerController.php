8f43bb43d19972184aa10342a4decd3466008e21
mernanagy <merna.nagy17@yahoo.com>
2016-05-26 16:19:54 +0200

    accepted answer back-end

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -5,8 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Answer;
 
 class AnswerController extends Controller
 {
     //
+    public function accept_answer(Request $request){
+
+            $id=$request->input('id');
+//            echo "id = ".$id;
+        $answer=Answer::find($id);
+        $answer->accepted=!($answer->accepted);
+        $answer->save();
+
+    }
 }

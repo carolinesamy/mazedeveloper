40210c0aae9875f9ab4ee085c64ec1743872ed33
ayaOS <ayaali35.aa@gmail.com>
2016-05-29 14:23:59 +0200

    handling views with returned data from http requests

--- public/js/coursecontroller.js
+++ public/js/coursecontroller.js
@@ -3,29 +3,29 @@ angular.module('developerMaze').controller('courseCtl',function( $scope ,$http, 
 	console.log($routeParams.id);
 	$scope.course_id = $routeParams.id;//here we need http request to get this course's data
 
-    $scope.course = {
-        'course_name':'php',
-        'course_id':$routeParams.id
-    }
+ //    $scope.course = {
+ //        'course_name':'php',
+ //        'course_id':$routeParams.id
+ //    }
 
-    $scope.courses =[
-	{
-		course_name:'PHP',
-	 	course_id:1
-	},
-	{
-		course_name:'Laravel',
-	 	course_id:2
-	},
-	{
-		course_name:'AngularJS',
-	 	course_id:4
-	},
-	{
-		course_name:'Bootstrap',
-	 	course_id:5
-	},
-	];
+ //    $scope.courses =[
+	// {
+	// 	course_name:'PHP',
+	//  	course_id:1
+	// },
+	// {
+	// 	course_name:'Laravel',
+	//  	course_id:2
+	// },
+	// {
+	// 	course_name:'AngularJS',
+	//  	course_id:4
+	// },
+	// {
+	// 	course_name:'Bootstrap',
+	//  	course_id:5
+	// },
+	// ];
 
 
 })
--- public/js/headercontroller.js
+++ public/js/headercontroller.js
@@ -3,40 +3,36 @@
 angular.module('developerMaze').controller('headerCtl',function( $scope,$location ,$http, $rootScope,sessionService){
 
 
-    $scope.question = {
-      'title':'',
-      'content':''
-    }
+    // $scope.question = {
+    //   'title':'',
+    //   'content':''
+    // }
 
-    $scope.user = {
-        'user_name':'',
-        'user_id':1,
-        'notification':0
-    }
 
-    $scope.notifications = [
-      {
-        'id':1,
-        'user':'aya',
-        'content':'answered your question'
-      },
-      {
-        'id':2,
-        'user':'caroline',
-        'content':'replied to your answer'
-      },
-      {
-        'id':4,
-        'user':'merna',
-        'content':'accept your answer'
-      },
-    ];
+    // $scope.notifications = [
+    //   {
+    //     'id':1,
+    //     'user':'aya',
+    //     'content':'answered your question'
+    //   },
+    //   {
+    //     'id':2,
+    //     'user':'caroline',
+    //     'content':'replied to your answer'
+    //   },
+    //   {
+    //     'id':4,
+    //     'user':'merna',
+    //     'content':'accept your answer'
+    //   },
+    // ];
 
-    $scope.courses = ['PHP','Bootstrap','Django','Java'];
+    // $scope.courses = ['PHP','Bootstrap','Django','Java'];
 
-    $scope.user.notification = $scope.notifications.length;
+ //   $scope.user.notification = $scope.notifications.length;
 
     $scope.sendData = function(valid){
+
       if(valid){
         $http({
             method: 'POST',
@@ -47,18 +43,22 @@ angular.module('developerMaze').controller('headerCtl',function( $scope,$locatio
         }).success(function(res){
 
             if(res.message=='password'){
-                console.log('pssword invalid');
+                alert('Invalid Password..');
                 $location.path('/');
             }
             else if (res.message=='email'){
-                console.log('email invalid');
+                alert('Invalid Email..');
                 $location.path('/');
             }
             else if(res.message=='login') {
-                console.log(res.user['id']);
+                //console.log(res.user);
+                $scope.user ={
+                    'name': res.user['sfull_name'],
+                    'id': res.user['id']
+                }
                 sessionService.set('user', res.user['id']);
                 sessionService.set('type',res.type);
-                $rootScope.currentuser = res;
+                $rootScope.currentuser = res.user;
                 $('#myModal').modal('hide');
                 $location.url('/questions');
             }
@@ -70,6 +70,7 @@ angular.module('developerMaze').controller('headerCtl',function( $scope,$locatio
     };
 
     $scope.logout = function(){
+
         sessionService.destroy('user');
         sessionService.destroy('type');
         $rootScope.currentuser = null;
@@ -81,7 +82,6 @@ angular.module('developerMaze').controller('headerCtl',function( $scope,$locatio
 
     $scope.askQuestion = function(valid){
 
-        console.log($scope.selected_tags);
         if(valid){
             $http({
                 method: 'POST',
@@ -93,7 +93,7 @@ angular.module('developerMaze').controller('headerCtl',function( $scope,$locatio
                     //'course_id':$scope.question.course,
                     //'tag_id':$scope.question.tag,
                     'course_id':1,
-                    'tag_id':[2,1],
+                    'tag_id':$scope.question.tags,
                     'student_id':sessionService.get('user')
                 }
             }).success(function(res){
@@ -103,12 +103,12 @@ angular.module('developerMaze').controller('headerCtl',function( $scope,$locatio
             }).error(function(err){
                 console.log(err);
             });
-          $scope.question = {
-                'title':'',
-                'content':'',
-                'image':'',
-                'student_id':''
-            };
+          // $scope.question = {
+          //       'title':'',
+          //       'content':'',
+          //       'image':'',
+          //       'student_id':''
+          //   };
         }
     };
 
